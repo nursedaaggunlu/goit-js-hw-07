5318311ccf9329158e3fa4563108749dba104fd0
Nur Seda Ağgünlü <nursedaaggunlu@gmail.com>
2025-11-05 21:36:42 +0300

    goit-js-hw-07 completed

---
+++ js/task-6.js
@@ -0,0 +1,50 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector("#input");
+const createBtn = document.querySelector("#create-btn");
+const destroyBtn = document.querySelector("#destroy-btn");
+const boxesContainer = document.querySelector("#boxes");
+
+ function createBoxes (amount) {
+    boxesContainer.innerHTML = "";
+
+    if(amount < 1 || amount > 100) {
+        alert("Please enter a number between 1 and 100");
+        return;
+    }
+
+    const boxes = [];
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+        const box = document.createElement("div");
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.margin = "5px";
+        box.style.display = "inline-block";
+        box.style.borderRadius ="4px";
+        boxes.push(box);
+        size += 10;
+    }
+
+    console.log(...boxes);
+
+    boxesContainer.append(boxes);
+ }
+
+ function destroyBoxes() {
+    boxesContainer.innerHTML = "";
+ }
+
+ createBtn.addEventListener("click", () => {
+    const amount = Number(input.value.trim());
+    createBoxes(amount);
+    input.value = "";
+ });
+
+ destroyBtn.addEventListener("click", destroyBoxes);
